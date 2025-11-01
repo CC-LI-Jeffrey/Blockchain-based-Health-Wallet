4d8d695587634fd42c696005ed1ac9c6a2386328
Jeffrey <jeffreyli12386@gmail.com>
2025-11-01 14:30:00 +0800

    Home Page UI

--- app/src/main/java/com/fyp/blockchainhealthwallet/MainActivity.kt
+++ app/src/main/java/com/fyp/blockchainhealthwallet/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.fyp.blockchainhealthwallet
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        // Set user name (in future, this will be fetched from blockchain/database)
+        findViewById<TextView>(R.id.tvUserName).text = "John Doe"
+
+        // Setup click listeners for all cards
+        findViewById<CardView>(R.id.cardMedication).setOnClickListener {
+            navigateToScreen("Medication Record")
+        }
+
+        findViewById<CardView>(R.id.cardVaccination).setOnClickListener {
+            navigateToScreen("Vaccination Record")
+        }
+
+        findViewById<CardView>(R.id.cardProfile).setOnClickListener {
+            navigateToScreen("Profile")
+        }
+
+        findViewById<CardView>(R.id.cardReports).setOnClickListener {
+            navigateToScreen("Reports")
+        }
+
+        findViewById<CardView>(R.id.cardShare).setOnClickListener {
+            navigateToScreen("Share")
+        }
+
+        findViewById<CardView>(R.id.cardAccessLogs).setOnClickListener {
+            navigateToScreen("Access Logs")
+        }
+
+        findViewById<CardView>(R.id.cardSettings).setOnClickListener {
+            navigateToScreen("Settings")
+        }
+    }
+
+    private fun navigateToScreen(screenName: String) {
+        // Placeholder for navigation - will be implemented when creating actual screens
+        Toast.makeText(this, "Navigating to $screenName", Toast.LENGTH_SHORT).show()
+        // TODO: Implement actual navigation using Intents or Navigation Component
     }
 }
